32a620dbe57ff954b3bcc3ea2ee52199457e6ab1
pinetcht <107372382+pinetcht@users.noreply.github.com>
2023-04-15 18:07:33 -0700

    Delete new-website directory

--- new-website/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Index() {
-    return (
-        <>
-            <h1>heyyy world!</h1>
-            <p>This is a starter template you can use for Web Dev for Makers projects.</p>
-            <img src="/logo.png" alt="Web Dev for Makers Logo" />
-        </>
-    );
-}
